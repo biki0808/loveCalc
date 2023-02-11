dd9922bb6e068e2de2a783c2090335af257332fe
Bikash Pradhan <100799701+biki0808@users.noreply.github.com>
2023-02-11 15:50:34 +0530

    uploaded

---
+++ calc.js
@@ -0,0 +1,49 @@
+function operation() {
+  var name = prompt("Enter Your Name");
+  var crush = prompt("Enter Your crush's Name");
+
+  if (name && crush) {
+    if (name == " " || crush == " ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "  " || crush == "  ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "   " || crush == "   ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "    " || crush == "    ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "     " || crush == "     ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "      " || crush == "      ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "       " || crush == "       ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "        " || crush == "        ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "         " || crush == "         ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "          " || crush == "          ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "           " || crush == "           ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "            " || crush == "            ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "             " || crush == "             ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "              " || crush == "              ") {
+      alert("IT is not valid, please enter your name");
+    } else if (name == "               " || crush == "               ") {
+      alert("IT is not valid, please enter your name");
+    } else {
+      function LoveCalc() {
+        var LovePercentage = Math.random();
+        LovePercentage = LovePercentage * 100;
+        LovePercentage = Math.floor(LovePercentage) + 1;
+        return LovePercentage;
+      }
+      document.getElementById("result").innerHTML =
+        "Your love percentage is " + LoveCalc() + "%.";
+    }
+  } else {
+    alert("It's ok if you don't want to calculate your love percentage.");
+  }
+}
